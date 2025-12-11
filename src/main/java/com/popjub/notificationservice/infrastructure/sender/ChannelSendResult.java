e0b04bc12ddebda4c7f60fcf0624945b8fd7b784
jessi87 <jessiseo87@gmail.com>
2025-12-12 02:25:40 +0900

    refactor: 파라미터 수정

--- src/main/java/com/popjub/notificationservice/infrastructure/sender/ChannelSendResult.java
+++ src/main/java/com/popjub/notificationservice/infrastructure/sender/ChannelSendResult.java
@@ -2,6 +2,7 @@ package com.popjub.notificationservice.infrastructure.sender;
 
 public record ChannelSendResult(
 	boolean success,
+	String requestBody,
 	String responseCode,
 	String responseBody,
 	String errorMessage
