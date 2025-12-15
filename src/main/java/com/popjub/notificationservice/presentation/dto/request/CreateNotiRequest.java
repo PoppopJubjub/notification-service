c8e41c4cc8836831cfcda9fca9ff9327a54a0322
jessi87 <jessiseo87@gmail.com>
2025-12-15 14:17:03 +0900

    chore: 미사용 처리

--- src/main/java/com/popjub/notificationservice/presentation/dto/request/CreateNotiRequest.java
+++ src/main/java/com/popjub/notificationservice/presentation/dto/request/CreateNotiRequest.java
@@ -2,7 +2,6 @@ package com.popjub.notificationservice.presentation.dto.request;
 
 import java.util.UUID;
 
-import com.popjub.notificationservice.application.dto.command.CreateNotiCommand;
 import com.popjub.notificationservice.domain.entity.ChannelType;
 import com.popjub.notificationservice.domain.entity.EventType;
 
